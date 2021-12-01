9ca267509aac0780c955e7f02b6abb07d7aac816
tomkosse <tom.kosse@infosupport.com>
2021-12-01 17:41:23 +0100

    Simpler LINQ-based solution

--- 01/solution/Program.cs
+++ 01/solution/Program.cs
@@ -9,29 +9,26 @@ namespace _01
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                throw new ArgumentException("missing first argument (file to process)");
+            }
+
             var numbers = File
                     .ReadAllLines(args[0])
                     .Select(str => int.Parse(str));
 
             INumberSelectionStrategy strategy = new SlidingWindowNumberSelectionStrategy();
             //INumberSelectionStrategy strategy = new AllNumberSelectionStrategy();
-                    
+
             var selectedNumbers = strategy.SelectNumbers(numbers);
 
-            int? previousNumber = null;
-            int larger = 0;
-            foreach (int number in selectedNumbers)
-            {
-                if (previousNumber.HasValue)
-                {
-                    if (number > previousNumber)
-                    {
-                        larger++;
-                    }
-                }
-                previousNumber = number;
-            }
-            System.Console.WriteLine(larger);
+            var outcome = selectedNumbers
+                .Skip(1)
+                .Zip(selectedNumbers, (current, previous) => current > previous ? 1 : 0)
+                .Sum();
+
+            System.Console.WriteLine(outcome);
         }
     }
 }
