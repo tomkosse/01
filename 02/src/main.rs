1602127be7c19679306b62950770ed5d8e6fc3de
tomkosse <tom.kosse@infosupport.com>
2021-12-02 09:40:24 +0100

    moved output out of loop

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -14,7 +14,6 @@ fn main() {
     let input = fs::read_to_string(&args[1])
         .expect("Couldn't read file");
 
-
     for line in input.lines() {
         let terms = line.split_whitespace().collect::<Vec<&str>>();
         let direction = terms[0];
@@ -30,20 +29,19 @@ fn main() {
             horpos += delta;
             depth += aim * delta;
         }
-
-        let mut output = "".to_owned();
-        output.push_str(&horpos.to_string());
-        output.push_str(" ");
-        output.push_str(&depth.to_string());
-        output.push_str(" ");
-        output.push_str(&aim.to_string());
-        output.push_str("=");
-        output.push_str(&(&horpos * &depth).to_string());
-
-        let stdout = stdout();
-        let mut writer = BufWriter::new(stdout.lock());
-        let outputwidth = output.chars().count();
-        say(output.as_bytes(), outputwidth, &mut writer).unwrap();
     }
 
+    let mut output = "".to_owned();
+    output.push_str(&horpos.to_string());
+    output.push_str(" ");
+    output.push_str(&depth.to_string());
+    output.push_str(" ");
+    output.push_str(&aim.to_string());
+    output.push_str("=");
+    output.push_str(&(&horpos * &depth).to_string());
+
+    let stdout = stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+    let outputwidth = output.chars().count();
+    say(output.as_bytes(), outputwidth, &mut writer).unwrap();
 }
