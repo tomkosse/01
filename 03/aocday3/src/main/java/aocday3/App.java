66cf7438bbd94098817fb9ccdcb0134dd8ef5406
tomkosse <tom.kosse@infosupport.com>
2021-12-03 11:26:36 +0100

    Formatted

--- 03/aocday3/src/main/java/aocday3/App.java
+++ 03/aocday3/src/main/java/aocday3/App.java
@@ -9,44 +9,45 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class App {
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
         var matrix = readFileIntoMatrix(args[0]);
-        
+
         var rotatedMatrix = rotateMatrix(matrix);
 
         var gamma = determineNumberByDiscriminator(rotatedMatrix, App::getMajorityNumber);
         var epsilon = determineNumberByDiscriminator(rotatedMatrix, App::getMinorityNumber);
 
         System.out.println("Gamma: " + gamma + " Epsilon: " + epsilon + " Power consumption: " + gamma * epsilon);
-        
+
         var o2genrating = bitsToNumber(reduceMatrixByDiscriminator(matrix, App::getMajorityNumber, 0).get(0));
         var co2scrubrating = bitsToNumber(reduceMatrixByDiscriminator(matrix, App::getMinorityNumber, 0).get(0));
-        System.out.println("O2 Generator Rating: " + o2genrating + " CO2 Scrubber Rating: " + co2scrubrating + " Life support rating:" + o2genrating * co2scrubrating);
+        System.out.println("O2 Generator Rating: " + o2genrating + " CO2 Scrubber Rating: " + co2scrubrating
+                + " Life support rating:" + o2genrating * co2scrubrating);
     }
 
     private static int bitsToNumber(ArrayList<Integer> bits) {
         int result = 0;
-        for(int i=0; i < bits.size(); i++)
-        {
+        for (int i = 0; i < bits.size(); i++) {
             result = result << 1;
             result += bits.get(i);
         }
         return result;
     }
 
-    private static ArrayList<ArrayList<Integer>> reduceMatrixByDiscriminator(ArrayList<ArrayList<Integer>> matrix, Function<ArrayList<Integer>, Integer> discriminatorFinder, int index) {
-        if(matrix.size() == 1)
-        {
+    private static ArrayList<ArrayList<Integer>> reduceMatrixByDiscriminator(ArrayList<ArrayList<Integer>> matrix,
+            Function<ArrayList<Integer>, Integer> discriminatorFinder, int index) {
+        if (matrix.size() == 1) {
             return matrix;
         }
 
         var rotatedMatrix = rotateMatrix(matrix);
 
         var bits = rotatedMatrix.get(index);
-        
+
         var discriminatorNumber = discriminatorFinder.apply(bits);
 
-        var reducedMatrix = matrix.stream().filter(line -> line.get(index) == discriminatorNumber).collect(Collectors.toCollection(ArrayList::new));    
+        var reducedMatrix = matrix.stream().filter(line -> line.get(index) == discriminatorNumber)
+                .collect(Collectors.toCollection(ArrayList::new));
         return reduceMatrixByDiscriminator(reducedMatrix, discriminatorFinder, index + 1);
     }
 
@@ -62,10 +63,10 @@ public class App {
         return zeroes <= ones ? 0 : 1; // Zeroes take precedent over ones if it comes to a tie
     }
 
-    private static int determineNumberByDiscriminator(ArrayList<ArrayList<Integer>> matrix, Function<ArrayList<Integer>, Integer> discriminatorFinder)  {
+    private static int determineNumberByDiscriminator(ArrayList<ArrayList<Integer>> matrix,
+            Function<ArrayList<Integer>, Integer> discriminatorFinder) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for(int i=0; i < matrix.size(); i++)
-        {
+        for (int i = 0; i < matrix.size(); i++) {
             var bits = matrix.get(i);
             int majorityNumber = discriminatorFinder.apply(bits);
             numbers.add(majorityNumber);
@@ -73,15 +74,13 @@ public class App {
         return bitsToNumber(numbers);
     }
 
-    private static ArrayList<ArrayList<Integer>> rotateMatrix(ArrayList<ArrayList<Integer>> lines){
+    private static ArrayList<ArrayList<Integer>> rotateMatrix(ArrayList<ArrayList<Integer>> lines) {
         var lineLength = lines.get(0).size(); // Assumes "square" (non-jagged) matrix
 
         ArrayList<ArrayList<Integer>> rotatedArray = new ArrayList<>();
-        for(int i=0; i < lineLength; i++)
-        {
+        for (int i = 0; i < lineLength; i++) {
             ArrayList<Integer> bits = new ArrayList<>();
-            for (ArrayList<Integer> line : lines) 
-            {
+            for (ArrayList<Integer> line : lines) {
                 int intVal = Integer.valueOf(line.get(i));
                 bits.add(intVal);
             }
@@ -96,7 +95,7 @@ public class App {
         try (var br = new BufferedReader(new FileReader(new File(filepath)))) {
             while (br.ready()) {
                 ArrayList<Integer> bits = new ArrayList<>();
-                for(char c : br.readLine().toCharArray()){
+                for (char c : br.readLine().toCharArray()) {
                     bits.add(c == 48 ? 0 : 1); // Converts char value for 0 to int 0. Assumes that it can only be 0 or 1
                 }
                 matrix.add(bits);
