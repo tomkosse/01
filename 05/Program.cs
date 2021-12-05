d847701dcf174e81f7c75d0ad5623f03c27adb5e
tomkosse <tom.kosse@infosupport.com>
2021-12-05 10:01:01 +0100

    Working verson of Day 5 in C#

---
+++ 05/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rangePairs = File
+                    .ReadAllLines(args[0])
+                    .Select(line => line.Split(" -> "))
+                    .Select(str =>
+                        (
+                            leftHand: str[0].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse),
+                            rightHand: str[1].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                        )
+                    );
+            var allPairs = rangePairs
+                            .Select(pair => pair.leftHand)
+                            .Union(rangePairs.Select(pair => pair.rightHand));
+            int globalMaxX = allPairs.Max(p => p.First()) + 1;
+            int globalMaxY = allPairs.Max(p => p.Last()) + 1;
+            
+            int[,] matrix = new int[globalMaxX, globalMaxY];
+            foreach (var rangePair in rangePairs)
+            {
+                var x1 = rangePair.leftHand.First();
+                var y1 = rangePair.leftHand.Last();
+                var x2 = rangePair.rightHand.First();
+                var y2 = rangePair.rightHand.Last();
+                var minY = Math.Min(y1, y2);
+                var maxY = Math.Max(y1, y2);
+                var minX = Math.Min(x1, x2);
+                var maxX = Math.Max(x1, x2);
+
+                if (x1 != x2 && y1 == y2)
+                {
+                    for (int i = minX; i <= maxX; i++)
+                    {
+                        matrix[i, y1]++;
+                    }
+                }
+                else if (y1 != y2 && x1 == x2)
+                {
+                    for (int i = minY; i <= maxY; i++)
+                    {
+                        matrix[x1, i]++;
+                    }
+                }
+                else
+                { // Diagonal
+                    int cursorY = y1;
+                    int cursorX = x1;
+                    while (cursorX != x2 && cursorY != y2)
+                    {
+                        matrix[cursorX, cursorY]++;
+                        cursorX += cursorX >= x2 ? -1 : 1;
+                        cursorY += cursorY >= y2 ? -1 : 1;
+                    }
+                    matrix[cursorX, cursorY]++;
+                }
+            }
+
+            int intersections = 0;
+            for (int i = 0; i < globalMaxY; i++)
+            {
+                for (int n = 0; n < globalMaxX; n++)
+                {
+                    var column = matrix[n, i];
+                    Console.Write(column);
+                    if (column > 1)
+                    {
+                        intersections++;
+                    }
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("Intersections: " + intersections);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Day 01 - C#
 Day 02 - Rust
 Day 03 - Java
 Day 04 - Java/ECMAScript with Node
-Day 05 - Go
+Day 05 - C# again..
 Day 06 - C
 Day 07 - Python
 Day 08 - Powershell
 Day 09 - Bash
 Day 10 - C++
-Day 11 - Scala
+Day 11 - Go
 Day 12 - Ruby
 Day 13 - Dart
 Day 14 - Swift
@@ -22,8 +22,8 @@ Day 17 - T-SQL
 Day 18 - TypeScript
 Day 19 - C#/Blazor
 Day 20 - PHP
+Day 21 - Scala
 
-Day 21 - TBD
 Day 22 - TBD
 Day 23 - TBD
 Day 24 - TBD
