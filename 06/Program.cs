9b0e1f46df903a2cb98935aff3b512b00339304e
tomkosse <tom.kosse@infosupport.com>
2021-12-06 11:35:28 +0100

    Advent of Code day 6 in C#

---
+++ 06/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lanternFish = File
+                .ReadAllLines(args[0])
+                .SelectMany(line => line.Split(","))
+                .Select(int.Parse)
+                .GroupBy(d => d)
+                .Select(bucket => new LanternFishBucket(bucket.Key, bucket.Count()));
+
+            School school = new School(lanternFish);
+
+            System.Console.WriteLine("Initial state " + school.ToString());
+            foreach (var iteration in Enumerable.Range(1, 256))
+            {
+                school.Cycle();
+                System.Console.WriteLine($"After {iteration} days: {school.ToString()}");
+            }
+            System.Console.WriteLine($"There are now {school.FishBuckets.Sum(fb => fb.AmountOfFish)} fish");
+        }
+
+        private class LanternFishBucket
+        {
+            public int Timer { get; private set; }
+            public long AmountOfFish { get; private set; }
+            public LanternFishBucket(int timer, long amountOfFish)
+            {
+                this.Timer = timer;
+                this.AmountOfFish = amountOfFish;
+            }
+            
+            // Returns true when the fish are spawning new fish
+            public bool DecreaseTimer()
+            {
+                if (Timer == 0)
+                {
+                    Timer = 6;
+                    return true;
+                }
+                Timer--;
+                return false;
+            }
+
+            public void AddFish(long amount)
+            {
+                AmountOfFish += amount;
+            }
+        }
+
+        private class School
+        {
+            public IList<LanternFishBucket> FishBuckets { get; }
+            public School(IEnumerable<LanternFishBucket> initialFishBucket)
+            {
+                FishBuckets = initialFishBucket.ToList();
+            }
+
+            public void SpawnLanternFish(long amount)
+            {
+                if (!FishBuckets.Any(fb => fb.Timer == 8))
+                {
+                    FishBuckets.Add(new LanternFishBucket(8, amount));
+                }
+                else
+                {
+                    FishBuckets
+                        .Where(fb => fb.Timer == 8)
+                        .Single()
+                        .AddFish(amount);
+                }
+            }
+
+            public void Cycle()
+            {
+                long amountToCreate = 0;
+                foreach (var fishBucket in FishBuckets)
+                {
+                    if (fishBucket.DecreaseTimer())
+                    {
+                        amountToCreate = fishBucket.AmountOfFish;
+                    }
+                }
+
+                SpawnLanternFish(amountToCreate);
+
+                var toMerge = FishBuckets.GroupBy(fb => fb.Timer).Where(f => f.Count() > 1);
+                foreach (var bucketToMerge in toMerge)
+                {
+                    var duplicates = bucketToMerge.Skip(1);
+                    bucketToMerge.First().AddFish(duplicates.Sum(b => b.AmountOfFish));
+                    duplicates.ToList().ForEach(fb => FishBuckets.Remove(fb));
+                }
+            }
+
+            public override String ToString()
+            {
+                return String.Join(',', FishBuckets.Select(f => f.Timer.ToString() + "x" + f.AmountOfFish));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Day 01 - C#
 Day 02 - Rust
 Day 03 - Java
 Day 04 - Java/ECMAScript with Node
-Day 05 - C# again..
-Day 06 - C
+Day 05 - C#
+Day 06 - C#
 Day 07 - Python
 Day 08 - Powershell
 Day 09 - Bash
@@ -23,8 +23,7 @@ Day 18 - TypeScript
 Day 19 - C#/Blazor
 Day 20 - PHP
 Day 21 - Scala
-
-Day 22 - TBD
+Day 22 - C
 Day 23 - TBD
 Day 24 - TBD
 Day 25 - TBD
