122111b26f4186d785c3f9c385e21e73d7853b49
tomkosse <tom.kosse@infosupport.com>
2021-12-13 12:06:26 +0100

    Another two lines eliminated

--- 13/Program.cs
+++ 13/Program.cs
@@ -12,23 +12,21 @@ namespace _13
             var lines = File.ReadLines(args[0]);
             IEnumerable<(int X, int Y)> dots = lines.Where(line => line.Contains(",")).Select(line => line.Split(",")).Select(arr => (X: int.Parse(arr[0]), Y: int.Parse(arr[1]))).ToArray();
 
-            var maxX = dots.Max(d => d.X);
-            var maxY = dots.Max(d => d.Y);
-
             foreach(var instruction in lines.Where(line => line.Contains("=")))
             {
                 int line = int.Parse(instruction.Split("=").Last().ToString());
                 if(instruction.Contains("x"))
                 {
                     dots = dots.FoldHorizontally(line);
-                    maxX -= line + 1;
                 }
                 else 
                 {
                     dots = dots.FoldVertically(line);
-                    maxY -= line + 1;
                 }
             }
+            
+            var maxX = dots.Max(d => d.X);
+            var maxY = dots.Max(d => d.Y);
             PrintDots(dots, maxX, maxY);
         }
 
