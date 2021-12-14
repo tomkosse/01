e76df48c04ce2718514bff1956fd98a418765659
tomkosse <tom.kosse@infosupport.com>
2021-12-14 12:26:55 +0100

    Part 2 day 14

--- 14/Program.cs
+++ 14/Program.cs
@@ -7,6 +7,7 @@ namespace _14
 {
     class Program
     {
+        private static Dictionary<char, long> _countPerCharacter = new Dictionary<char, long>();
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines(args[0]);
@@ -26,38 +27,76 @@ namespace _14
             }
 
             System.Console.WriteLine("Template:     " + template);
-            Queue<char> characters = new Queue<char>(template.ToCharArray());
-            var charList = new Queue<char>();
+            var pairCounts = new Dictionary<string, long>();
+
+            foreach(var c in template)
+            {
+                AddToTally(c, 1);
+            }
+
+            while (template.Length > 1)
+            {
+                var pair = template.Substring(0, 2);
+                pairCounts.Add(pair, 1);
+                System.Console.WriteLine("Added pair " + pair);
+                template = template.Substring(1);
+            }
 
             int maxSteps = 40;
             var start = DateTime.Now;
             for (int i = 1; i <= maxSteps; i++)
             {
-                while (characters.Count > 1)
+                Dictionary<string, long> toInsert = new Dictionary<string, long>();
+                foreach(var pair in pairCounts.Keys)
                 {
-                    var char1 = characters.Dequeue();
-                    var char2 = characters.Peek();
-                    var match = lookup[char1][char2];
-                    charList.Enqueue(char1);
-                    charList.Enqueue(match);
-                }
-                charList.Enqueue(characters.Dequeue());
-                var elapsed = (DateTime.Now - start).TotalMilliseconds;
-                characters = charList;
+                    var match = lookup[pair[0]][pair[1]];
+                    var newPair1 = pair[0].ToString() + match.ToString();
+                    var newPair2 = match.ToString() + pair[1].ToString();
+                    if(!toInsert.ContainsKey(newPair1))
+                    {
+                        toInsert.Add(newPair1, 0);
+                    }
+                    if(!toInsert.ContainsKey(newPair2))
+                    {
+                        toInsert.Add(newPair2, 0);
+                    }
+                    toInsert[newPair1] += pairCounts[pair];
+                    toInsert[newPair2] += pairCounts[pair];
+                    
+                    AddToTally(match, pairCounts[pair]);
 
-                System.Console.WriteLine($"After step {i}: {charList.Count}  - elapsed: " + elapsed);
-                if (i != maxSteps)
-                {
-                    charList = new Queue<char>();
+                    pairCounts[pair] -= pairCounts[pair];
+                    if(pairCounts[pair] == 0)
+                    {
+                        pairCounts.Remove(pair);
+                    }
+                    System.Console.WriteLine("Replacing " + pair + " with " + newPair1 + " and " + newPair2);
                 }
+
+                toInsert.ToList().ForEach(ti => { if(pairCounts.ContainsKey(ti.Key)) { pairCounts[ti.Key] += ti.Value; } else { pairCounts.Add(ti.Key, ti.Value); } });
+
+                var elapsed = (DateTime.Now - start).TotalMilliseconds;
+                System.Console.WriteLine($"After step {i}: {pairCounts.Sum(pc => pc.Value) + 1}  - elapsed: " + elapsed);
+                pairCounts.ToList().ForEach(pc => System.Console.WriteLine(pc.Key + " - " + pc.Value));
             }
 
-            var countedOccurrences = new string(charList.ToArray()).GroupBy(c => c).Select(g => (character: g.Key, count: g.Count())).OrderBy(el => el.count);
-            var mostCommon = countedOccurrences.Last();
-            var leastCommon = countedOccurrences.First();
+            var mostCommon = _countPerCharacter.OrderBy(pc => pc.Value).Last();
+            var leastCommon = _countPerCharacter.OrderBy(pc => pc.Value).First();
 
-            var score = mostCommon.count - leastCommon.count;
-            System.Console.WriteLine($"Score: {score} Most common: {mostCommon.character}({mostCommon.count}) least common: {leastCommon.character}({leastCommon.count})");
+            var score = mostCommon.Value - leastCommon.Value;
+            System.Console.WriteLine($"Score: {score} Most common: {mostCommon.Key}({mostCommon.Value}) least common: {leastCommon.Key}({leastCommon.Value})");
+        }
+
+        private static void AddToTally(char c, long amount)
+        {
+            if (!_countPerCharacter.ContainsKey(c))
+            {
+                _countPerCharacter.Add(c, amount);
+            }
+            else
+            {
+                _countPerCharacter[c] += amount;
+            }
         }
     }
 }
