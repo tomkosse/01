83c1e01bb019b781cbd2f7637058379217f3c388
tomkosse <tom.kosse@infosupport.com>
2021-12-14 13:05:33 +0100

    Shortened further

--- 14/Program.cs
+++ 14/Program.cs
@@ -5,97 +5,56 @@ using System.Linq;
 
 namespace _14
 {
-    class Program
+    static class Program
     {
-        private static Dictionary<char, long> _countPerCharacter = new Dictionary<char, long>();
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines(args[0]);
-            var template = lines.First();
-
-            var insertions = lines.Where(l => l.Contains("->")).Select(l => l.Split(" -> ")).Select(arr => (searchPattern1: arr[0][0], searchPattern2: arr[0][1], element: arr[1][0])).ToArray();
-            var lookup = new Dictionary<char, Dictionary<char, char>>();
-            foreach (var insertion in insertions)
-            {
-                if (!lookup.ContainsKey(insertion.searchPattern1))
-                {
-                    var innerDict = new Dictionary<char, char>();
-                    lookup.Add(insertion.searchPattern1, innerDict);
-                }
-                var innerDictionary = lookup[insertion.searchPattern1];
-                innerDictionary.Add(insertion.searchPattern2, insertion.element);
-            }
-
-            System.Console.WriteLine("Template:     " + template);
-            var pairCounts = new Dictionary<string, long>();
-
-            foreach(var c in template)
-            {
-                AddToTally(c, 1);
-            }
-
-            while (template.Length > 1)
-            {
-                var pair = template.Substring(0, 2);
-                pairCounts.Add(pair, 1);
-                System.Console.WriteLine("Added pair " + pair);
-                template = template.Substring(1);
-            }
-
-            int maxSteps = 40;
+            var lines = File.ReadAllLines(args[0]).ToArray();
             var start = DateTime.Now;
+
+            var template = lines[0];
+            var lookup = lines.Skip(2).Select(l => l.Split(" -> ")).ToDictionary(i => i[0], i => i[1][0]);
+
+            var pairCounts = new Dictionary<string, long>();
+            for (int i = 0; i < template.Length - 1; i++)
+            {
+                var pair = template[i].ToString() + template[i + 1];
+                pairCounts.Add(pair, 1);
+            }
+
+            var countPerCharacter = new Dictionary<char, long>();
+            template.ToList().ForEach(c => countPerCharacter.EnterOrAdd(c, 1));
+
+            int maxSteps = 40; // 10 for part 1
             for (int i = 1; i <= maxSteps; i++)
             {
                 Dictionary<string, long> toInsert = new Dictionary<string, long>();
-                foreach(var pair in pairCounts.Keys)
+                foreach (var pair in pairCounts.Keys.ToList())
                 {
-                    var match = lookup[pair[0]][pair[1]];
-                    var newPair1 = pair[0].ToString() + match.ToString();
-                    var newPair2 = match.ToString() + pair[1].ToString();
-                    if(!toInsert.ContainsKey(newPair1))
-                    {
-                        toInsert.Add(newPair1, 0);
-                    }
-                    if(!toInsert.ContainsKey(newPair2))
-                    {
-                        toInsert.Add(newPair2, 0);
-                    }
-                    toInsert[newPair1] += pairCounts[pair];
-                    toInsert[newPair2] += pairCounts[pair];
-                    
-                    AddToTally(match, pairCounts[pair]);
-
-                    pairCounts[pair] -= pairCounts[pair];
-                    if(pairCounts[pair] == 0)
-                    {
-                        pairCounts.Remove(pair);
-                    }
-                    System.Console.WriteLine("Replacing " + pair + " with " + newPair1 + " and " + newPair2);
+                    toInsert.EnterOrAdd(pair[0].ToString() + lookup[pair].ToString(), pairCounts[pair]);
+                    toInsert.EnterOrAdd(lookup[pair].ToString() + pair[1].ToString(), pairCounts[pair]);
+                    countPerCharacter.EnterOrAdd(lookup[pair], pairCounts[pair]);
+                    pairCounts.Remove(pair);
                 }
-
-                toInsert.ToList().ForEach(ti => { if(pairCounts.ContainsKey(ti.Key)) { pairCounts[ti.Key] += ti.Value; } else { pairCounts.Add(ti.Key, ti.Value); } });
-
-                var elapsed = (DateTime.Now - start).TotalMilliseconds;
-                System.Console.WriteLine($"After step {i}: {pairCounts.Sum(pc => pc.Value) + 1}  - elapsed: " + elapsed);
-                pairCounts.ToList().ForEach(pc => System.Console.WriteLine(pc.Key + " - " + pc.Value));
+                foreach (var ti in toInsert)
+                {
+                    pairCounts.EnterOrAdd(ti.Key, ti.Value);
+                }
             }
-
-            var mostCommon = _countPerCharacter.OrderBy(pc => pc.Value).Last();
-            var leastCommon = _countPerCharacter.OrderBy(pc => pc.Value).First();
-
-            var score = mostCommon.Value - leastCommon.Value;
-            System.Console.WriteLine($"Score: {score} Most common: {mostCommon.Key}({mostCommon.Value}) least common: {leastCommon.Key}({leastCommon.Value})");
+            var ordered = countPerCharacter.OrderBy(pc => pc.Value).ToArray();
+            var score = ordered[^1].Value - ordered[0].Value;
+            System.Console.WriteLine($"Score: {score} - elapsed: {(DateTime.Now - start).TotalMilliseconds}");
         }
 
-        private static void AddToTally(char c, long amount)
+        public static void EnterOrAdd<K>(this Dictionary<K, long> dict, K key, long value)
         {
-            if (!_countPerCharacter.ContainsKey(c))
+            if (!dict.ContainsKey(key))
             {
-                _countPerCharacter.Add(c, amount);
+                dict.Add(key, value);
             }
             else
             {
-                _countPerCharacter[c] += amount;
+                dict[key] = dict[key] + value;
             }
         }
     }
