06c330e9402bc02cfdda58fc3cad4e053f7f0038
tomkosse <tom.kosse@infosupport.com>
2021-12-17 12:21:40 +0100

    shortened

--- 17/Program.cs
+++ 17/Program.cs
@@ -16,62 +16,47 @@ namespace _17
             var xRange = coordSpace[0];
             var yRange = coordSpace[1];
 
-            var maximumYPosition = int.MinValue;
-            var maxSettings = (xVel: 0, yVel: 0);
-            var allValidVelocities = new List<(int xVel, int yVel)>();
-            for (int x = 0; x < xRange[1] + 1; x++)
+            var maxSettings = (maxHeight: int.MinValue, xVel: 0, yVel: 0);
+            int amountOfValidVelocities = 0;
+            var lowerYBound = -1 * (Math.Abs(yRange[0]));
+            var higherYBound = Math.Abs(yRange[0]);
+            for (int x = 1; x <= xRange[1]; x++)
             {
-                var lowerYBound = -1 * (Math.Abs(yRange[1] * 2));
-                var higherYBound = Math.Abs(yRange[1] * 2);
                 for (int y = lowerYBound; y < higherYBound; y++)
                 {
-                    int highestY = int.MinValue;
+                    int highestHeight = int.MinValue;
                     var probe = (X: 0, Y: 0);
-
                     var velocity = (xVel: x, yVel: y);
 
-                    while (HasNotOvershot(probe, velocity, xRange[1], yRange[1]))
+                    while (probe.X <= xRange[1] && probe.Y >= yRange[0])
                     {
                         probe.X += velocity.xVel;
                         probe.Y += velocity.yVel;
-                        var towardsZero = -1 * velocity.xVel.CompareTo(0);
-                        velocity.xVel += towardsZero;
+                        velocity.xVel += -1 * velocity.xVel.CompareTo(0);
                         velocity.yVel--;
-                        if (probe.Y > highestY)
+                        if (probe.Y > highestHeight)
                         {
-                            highestY = probe.Y;
+                            highestHeight = probe.Y;
                         }
 
                         if (IsInRange(probe.X, xRange[0], xRange[1]) && IsInRange(probe.Y, yRange[0], yRange[1]))
                         {
-                            if (highestY > maximumYPosition)
+                            if (highestHeight > maxSettings.maxHeight)
                             {
-                                maximumYPosition = highestY;
-                                maxSettings = (xVel: x, yVel: y);
+                                maxSettings = (maxHeight: highestHeight, xVel: x, yVel: y);
                             }
-                            allValidVelocities.Add((xVel: x, yVel: y));
+                            amountOfValidVelocities++;
                             break;
                         }
                     }
                 }
             }
             var time = sw.ElapsedMilliseconds;
-            System.Console.WriteLine(allValidVelocities.Count);
-            System.Console.WriteLine($"Reached {maximumYPosition} with velocity {maxSettings.xVel},{maxSettings.yVel}");
+            System.Console.WriteLine(amountOfValidVelocities);
+            System.Console.WriteLine($"Reached {maxSettings.maxHeight} with velocity {maxSettings.xVel},{maxSettings.yVel}");
             System.Console.WriteLine("Done in " + time + "ms");
         }
 
-        private static bool HasNotOvershot((int X, int Y) probe, (int xVel, int yVel) velocity, int furthestXValue, int furthestYValue)
-        {
-            var nextXPoint = (probe.X + velocity.xVel);
-            var nextYPoint = (probe.Y + velocity.yVel);
-            //return probe.X <= furthestXPoint || Math.Abs(furthestYPoint - nextYPoint) < Math.Abs(furthestYPoint - probe.Y);
-            return probe.X <= furthestXValue && IsInRange(probe.Y, -10000, 10000);
-        }
-
-        public static bool IsInRange(int a, int min, int max)
-        {
-            return a >= min && a <= max;
-        }
+        public static bool IsInRange(int a, int min, int max) => a >= min && a <= max;
     }
 }
