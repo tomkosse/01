9f61097798ecccc64e8b187799e9026628c1d6f3
tomkosse <tom.kosse@infosupport.com>
2021-12-19 15:00:47 +0100

    checkpoint

--- 19/Program.cs
+++ 19/Program.cs
@@ -11,7 +11,7 @@ namespace _19
         {
             var input = File.ReadAllLines(args[0]);
 
-            List<(int scanner, List<(int x, int y, int z)> coords)> reports = new List<(int scanner, List<(int x, int y, int z)> coords)>();
+            List<List<(int x, int y, int z)>> reports = new List<List<(int x, int y, int z)>>();
             var inputQueue = new Queue<string>(input);
             while (inputQueue.Any())
             {
@@ -26,91 +26,91 @@ namespace _19
                     }
                     list.Add(StringToCoords(coord));
                 }
-                reports.Add((scannerId, list));
+                reports.Add(list);
             }
 
             DeduceMap(reports);
         }
 
-        private static void DeduceMap(List<(int scanner, List<(int x, int y, int z)> coords)> reports)
+        private static void DeduceMap(List<List<(int x, int y, int z)>> reports)
         {
             var locations = new Nullable<(int x, int y, int z)>[reports.Count];
             var baseReport = reports[0];
             locations[0] = (0, 0, 0);
-            for (int i = 1; i < reports.Count; i++)
+            while (locations.Any(l => l == null))
             {
-                var scannerLocation = MakeRelativeToSameBeacon(baseReport, reports[i]);
-                locations[i] = scannerLocation;
-                if (scannerLocation.HasValue)
+                for (int i = 0; i < reports.Count; i++)
                 {
-                    System.Console.WriteLine("Found " + i + " at ");
-                    PrintCoord(scannerLocation.Value);
+                    for (int j = 0; j < reports.Count; j++)
+                    {
+                        if (locations[j] == null && locations[i] != null)
+                        {
+                            var scannerLocation = MakeRelativeToSameBeacon(reports[i], reports[j]);
+                            if (scannerLocation.HasValue)
+                            {
+                                System.Console.WriteLine("Found " + j + " by comparing to " + i + " at ");
+                                PrintCoord(scannerLocation.Value.coord);
+
+                                var reverseTransformation = MakeRelativeToSameBeacon(reports[j], reports[i]).Value.perspectiveId;
+                                System.Console.WriteLine("Reverse = " + reverseTransformation);
+                                var reversed = reports[j].Select(c => GetAbsoluteCoordinate(scannerLocation.Value.coord, GetPerspectives(c)[reverseTransformation])).ToList();
+
+                                if (reports[i].Intersect(reversed).Count() >= 12)
+                                {
+                                    reports[j] = reversed;
+                                    locations[j] = scannerLocation.Value.coord;
+                                }
+                                else
+                                {
+                                    System.Console.WriteLine("NOPE");
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        private static Nullable<(int x, int y, int z)> MakeRelativeToSameBeacon((int scanner, List<(int x, int y, int z)> coords) firstReport, (int scanner, List<(int x, int y, int z)> coords) otherReport)
+        private static Nullable<(int perspectiveId, (int x, int y, int z) coord)> MakeRelativeToSameBeacon(List<(int x, int y, int z)> firstReport, List<(int x, int y, int z)> otherReport)
         {
-            var requiredMatches = firstReport.coords.Any(fr => fr.z != 0) ? 11 : 2;
-            for (int j = 0; j < firstReport.coords.Count; j++)
+            var requiredMatches = firstReport.Any(fr => fr.z != 0) ? 11 : 2;
+            for (int j = 0; j < firstReport.Count; j++)
             {
                 List<(int x, int y, int z)> list = new List<(int x, int y, int z)>();
-                var referenceBeacon = firstReport.coords[j];
-                foreach (var otherBeacon in firstReport.coords.Except(new [] { referenceBeacon }))
+                var referenceBeacon = firstReport[j];
+                foreach (var otherBeacon in firstReport.Except(new[] { referenceBeacon }))
                 {
                     var coord = GetRelativeCoordinate(referenceBeacon, otherBeacon);
                     list.Add(coord);
                 }
 
-                if(j ==0)
-                {
-                    list.ForEach(PrintCoord);
-                }
-                System.Console.WriteLine("-");
-
                 // All beacons in the first report are now relative to a beacon. Let's see if we can find the same beacon in another report
-                for (int i = 0; i < otherReport.coords.Count; i++)
+                for (int i = 0; i < otherReport.Count; i++)
                 {
-                    var fb = otherReport.coords[i];
-                    var otherCoords = otherReport.coords.Except(new[] { fb });
+                    var fb = otherReport[i];
+                    var otherCoords = otherReport.Except(new[] { fb });
 
                     List<(int perspectiveId, int x, int y, int z)> list2 = new List<(int perspectiveId, int x, int y, int z)>();
                     foreach (var otherBeacon in otherCoords)
                     {
                         var relCoord = GetRelativeCoordinate(fb, otherBeacon);
-                        var possibleCoord = GeneratePerspectives(relCoord);
-                        for(int perspectiveId = 0; perspectiveId < possibleCoord.Length; perspectiveId++)
+                        var possibleCoord = GetPerspectives(relCoord);
+                        for (int perspectiveId = 0; perspectiveId < possibleCoord.Length; perspectiveId++)
                         {
-                            list2.Add((perspectiveId, possibleCoord[perspectiveId].x, possibleCoord[perspectiveId].y, possibleCoord[perspectiveId].z));
-                        }
-                    }
+                            if (list.Contains((possibleCoord[perspectiveId].x, possibleCoord[perspectiveId].y, possibleCoord[perspectiveId].z)))
+                            {
+                                list2.Add((perspectiveId, possibleCoord[perspectiveId].x, possibleCoord[perspectiveId].y, possibleCoord[perspectiveId].z));
 
-                    var matches = Intersect(list, list2);
-                    
-                    if (matches.Count() >= requiredMatches)
-                    {
-                        var perspectiveId = matches.First().perspectiveId;
-                        var possibleCoord = GeneratePerspectives(fb)[perspectiveId];
-                        return GetRelativeCoordinate(possibleCoord, referenceBeacon);
+                                if (list2.Count() >= requiredMatches)
+                                {
+                                    return (perspectiveId, GetRelativeCoordinate(GetPerspectives(fb)[perspectiveId], referenceBeacon));
+                                }
+                            }
+                        }
                     }
                 }
             }
             return null;
-        }
-        
-        private static IEnumerable<(int perspectiveId, int x, int y, int z)> Intersect(List<(int x, int y, int z)> list, List<(int perspectiveId, int x, int y, int z)> list2)
-        {
-            foreach(var i in list2)
-            {
-                foreach(var j in list)
-                {
-                    if(j.x == i.x && j.y == i.y && j.z == i.z)
-                    {
-                        yield return i;
-                    }
-                }
-            }
-
         }
 
         private static void PrintCoord((int x, int y, int z) coord)
@@ -123,29 +123,31 @@ namespace _19
             return (x: otherBeacon.x - firstBeacon.x, y: otherBeacon.y - firstBeacon.y, z: otherBeacon.z - firstBeacon.z);
         }
 
-        private static (int x, int y, int z)[] GeneratePerspectives((int x, int y, int z) coord)
+        private static (int x, int y, int z) GetAbsoluteCoordinate((int x, int y, int z) referenceOffset, (int x, int y, int z) beacon)
+        {
+            return (x: referenceOffset.x + beacon.x, y: referenceOffset.y + beacon.y, z: referenceOffset.z + beacon.z);
+        }
+
+        private static (int x, int y, int z)[] GetPerspectives((int x, int y, int z) coord)
         {
             return new (int x, int y, int z)[]
             {
                 (x: coord.x * -1, y: coord.y, z: coord.z * -1),
-                (x: coord.x * -1, y: coord.y, z: coord.z),
-
                 (x: coord.x, y: coord.y, z: coord.z),
+                (x: coord.x * -1, y: coord.y, z: coord.z),
                 (x: coord.x, y: coord.y * -1, z: coord.z),
                 (x: coord.x, y: coord.y, z: coord.z * -1),
                 (x: coord.x * -1, y: coord.y * -1, z: coord.z),
                 (x: coord.x, y: coord.y * -1, z: coord.z * -1),
                 (x: coord.x * -1, y: coord.y * -1, z: coord.z * -1),
-
                 (x: coord.x, y: coord.z, z: coord.y),
                 (x: coord.x * -1, y: coord.z, z: coord.y),
                 (x: coord.x, y: coord.z * -1, z: coord.y),
                 (x: coord.x, y: coord.z, z: coord.y * -1),
                 (x: coord.x * -1, y: coord.z * -1, z: coord.y),
-                (x: coord.x* -1, y: coord.z, z: coord.y * -1),
+                (x: coord.x * -1, y: coord.z, z: coord.y * -1),
                 (x: coord.x, y: coord.z * -1, z: coord.y * -1),
                 (x: coord.x * -1, y: coord.z * -1, z: coord.y * -1),
-
                 (x: coord.z, y: coord.x, z: coord.y),
                 (x: coord.z * -1, y: coord.x, z: coord.y),
                 (x: coord.z, y: coord.x * -1, z: coord.y),
@@ -154,7 +156,6 @@ namespace _19
                 (x: coord.z * -1, y: coord.x, z: coord.y * -1),
                 (x: coord.z, y: coord.x * -1, z: coord.y * -1),
                 (x: coord.z * -1, y: coord.x * -1, z: coord.y * -1),
-
                 (x: coord.z, y: coord.y, z: coord.x),
                 (x: coord.z * -1, y: coord.y, z: coord.x),
                 (x: coord.z, y: coord.y * -1, z: coord.x),
@@ -163,7 +164,6 @@ namespace _19
                 (x: coord.z * -1, y: coord.y, z: coord.x * -1),
                 (x: coord.z, y: coord.y * -1, z: coord.x * -1),
                 (x: coord.z * -1, y: coord.y * -1, z: coord.x * -1),
-
                 (x: coord.y, y: coord.z, z: coord.x),
                 (x: coord.y * -1, y: coord.z, z: coord.x),
                 (x: coord.y, y: coord.z * -1, z: coord.x),
@@ -172,7 +172,6 @@ namespace _19
                 (x: coord.y, y: coord.z * -1, z: coord.x * -1),
                 (x: coord.y * -1, y: coord.z, z: coord.x * -1),
                 (x: coord.y * -1, y: coord.z * -1, z: coord.x * -1),
-
                 (x: coord.y, y: coord.x, z: coord.z),
                 (x: coord.y * -1, y: coord.x, z: coord.z),
                 (x: coord.y, y: coord.x * -1, z: coord.z),
