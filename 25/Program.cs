8e3928cf6fbe76cec4fe4abee9750b1b4064fb92
tomkosse <tom.kosse@infosupport.com>
2021-12-25 06:54:55 +0100

    more cleanup

--- 25/Program.cs
+++ 25/Program.cs
@@ -3,11 +3,11 @@
 var lines = File.ReadAllLines(args[0]);
 var sw = Stopwatch.StartNew();
 
-var seabed = GetCellMatrix(lines.Select(lines => lines.ToCharArray().Select(c => c).ToArray()).ToArray());
+var allCells = GetCellMatrix(lines.Select(lines => lines.ToCharArray()).ToArray());
 
 int countMoved = 1;
 int step = 0;
-var allCells = seabed.SelectMany(c => c).ToArray();
+
 while (countMoved > 0)
 {
     countMoved = 0;
@@ -20,7 +20,7 @@ while (countMoved > 0)
 sw.Stop();
 System.Console.WriteLine($"Done! {step} in {sw.ElapsedMilliseconds}ms");
 
-static Cell[][] GetCellMatrix(char[][] numberMatrix)
+static Cell[] GetCellMatrix(char[][] numberMatrix)
 {
     Cell[][] cells = new Cell[numberMatrix.Length][];
 
@@ -41,31 +41,13 @@ static Cell[][] GetCellMatrix(char[][] numberMatrix)
         cell.East = neighbours.east;
         cell.South = neighbours.south;
     }
-    return cells;
+    return cells.SelectMany(o => o).ToArray();
 }
 
 static (Cell south, Cell east) GetAdjecentCells(Cell[][] cellMatrix, int y, int x)
 {
-    List<Cell> cells = new List<Cell>();
-    var row = cellMatrix[y];
-    Cell south;
-    if (y < cellMatrix.Length - 1)
-    {
-        south = cellMatrix[y + 1][x];
-    }
-    else
-    {
-        south = cellMatrix[0][x];
-    }
-    Cell east;
-    if (x < row.Length - 1)
-    {
-        east = cellMatrix[y][x + 1];
-    }
-    else
-    {
-        east = cellMatrix[y][0];
-    }
+    Cell south = y < cellMatrix.Length - 1 ? cellMatrix[y + 1][x] : cellMatrix[0][x];
+    Cell east = x < cellMatrix[y].Length - 1 ? cellMatrix[y][x + 1] : cellMatrix[y][0];
     return (south, east);
 }
 
@@ -76,44 +58,21 @@ public class Cell
     public int X { get; }
     public int Y { get; }
 
-    public bool Occupied { get { return Value.HasValue; } }
+    public bool Occupied => Value.HasValue;
 
     public Cell South { get; set; }
     public Cell East { get; set; }
 
-    public bool Eastbound
-    {
-        get
-        {
-            return Value == '>';
-        }
-    }
-    public bool Southbound
-    {
-        get
-        {
-            return Value == 'v';
-        }
-    }
+    public bool Eastbound => Value == '>';
 
-    public bool CanMove
-    {
-        get
-        {
-            return (Eastbound && !East.Occupied) || (Southbound && !South.Occupied);
-        }
-    }
+    public bool Southbound => Value == 'v';
+
+    public bool CanMove => (Eastbound && !East.Occupied) || (Southbound && !South.Occupied);
 
     public Cell(char value, int x, int y)
     {
-        if (value == '.')
-        {
-            Value = null;
-        }
-        else
-        {
-            Value = value;
-        }
+        Value = value == '.' ? null : value;
+
         X = x;
         Y = y;
     }
